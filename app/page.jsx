091a6c1eb230cdb2c92a8e686111fdddbe337889
Deveshpoojary <deveshpoojary05@gmail.com>
2025-04-02 19:40:07 +0530

    h

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import Headerlogo from "@/components/headerlogo";
+// import Headerlogo from "@/components/headerlogo";
 import Headername from "@/components/headername";
 import Search from "@/components/GlowingSearch";
 
@@ -6,7 +6,7 @@ import Search from "@/components/GlowingSearch";
 export default function Home() {
   return (
     <main className="p-4">
-      <Headerlogo />
+      {/* <Headerlogo /> */}
       <Headername />
       <Search />
       <h1 className="text-2xl font-bold"> Welcome to zizten</h1>
